0737e50c3e9f16a6b9d6f9fd42b071d17d8529da
andrei_akhmetshin <illife@yandex.ru>
2024-12-24 19:59:49 +0300

    Создание сервиса file_sync

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+from cloud_storage import CloudStorage
+from utils import list_files, sync
+import configparser
+import logging
+
+# Загрузка конфигурации
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+local_folder = config['Settings']['local_folder']
+cloud_folder = config['Settings']['cloud_folder']
+token = config['Settings']['access_token']
+sync_interval = int(config['Settings']['sync_interval'])
+
+# Инициализация
+storage = CloudStorage(token, cloud_folder, local_folder)
+
+
+def main():
+    logging.info("Сервис синхронизации запущен.")
+    try:
+        while True:
+            local_files = list_files(local_folder)
+            cloud_files = storage.get_info()
+            if local_files and cloud_files:
+                sync(local_files, cloud_files, storage)
+            else:
+                logging.error("Не удалось получить список файлов. Синхронизация пропущена.")
+            time.sleep(sync_interval)
+    except KeyboardInterrupt:
+        logging.info("Сервис синхронизации остановлен.")
+
+if __name__ == "__main__":
+    main()
